8a0b437fb5cb0443d846f421ced4964ef810a5cd
Misikir <mikireyob@gmail.com>
2019-02-06 00:38:53 -0500

    Server class and Event named tuple created

--- server_resources.py
+++ server_resources.py
@@ -1,4 +1,7 @@
-from collections import deque
+from collections import deque, namedtuple
+
+Event = namedtuple('Event', 'creator type time')
+
 
 class Queue():
     def __init__(self, queue_id):
@@ -18,6 +21,22 @@ class Worker():
         self._worker_id = worker_id
         self._capacity = capacity
         self.state = 'i'
-    def process_job(self, job, start_time):
+    def process_job(self, job, current_time):
         self.state = 'w'
-        return start_time + (job.get_size() / self._capacity)
+        return Event(self._worker_id, 'job_complete', current_time + (job.get_size() / self._capacity))
+
+class Server(): 
+    id_seq = 1
+    def __init__(self, launch_delay,num_of_workers, worker_capacity):
+        self.server_id = 'S' + str(Server.id_seq)
+        Server.id_seq += 1
+        self.launch_delay = launch_delay
+        self.queue = Queue(self.server_id + 'Q')
+        self.workers = {}
+        for w_id in range(1, num_of_workers + 1):
+            worker_id = self.server_id + 'W' + str(w_id)
+            self.workers[worker_id] = Worker(worker_id, worker_capacity)
+    def start(self, current_time):
+        self.state = 'launching'
+        return Event(self.server_id, 'launch_complete', current_time + self.launch_delay)
+
