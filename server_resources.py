befac83c0c798e91f7fd2598a1009dcd6706847a
Misikir <mikireyob@gmail.com>
2019-02-05 21:28:10 -0500

    worker class created

--- server_resources.py
+++ server_resources.py
@@ -11,3 +11,13 @@ class Queue():
             return self._queue.popleft()
         except IndexError:
             return None
+
+
+class Worker():
+    def __init__(self, worker_id, capacity):
+        self._worker_id = worker_id
+        self._capacity = capacity
+        self.state = 'i'
+    def process_job(self, job, start_time):
+        self.state = 'w'
+        return start_time + (job.get_size() / self._capacity)
