4004303626b8e6282841deaf0a08ae30689f1c69
Misikir <mikireyob@gmail.com>
2019-02-05 20:46:34 -0500

    modified Queue class

--- server_resources.py
+++ server_resources.py
@@ -1,16 +1,13 @@
 from collections import deque
 
 class Queue():
-    queue_id = 1
-    def __init__(self):
-        self.q_id = Queue.queue_id
-        Queue.queue_id += 1
-        self.num_jobs = 0
-        self.queue = deque()
+    def __init__(self, queue_id):
+        self._queue_id = queue_id
+        self._queue = deque()
     def put_job(self, job):
-        self.queue.append(job)
+        self._queue.append(job)
     def get_job(self):
         try:
-            return self.queue.popleft()
+            return self._queue.popleft()
         except IndexError:
             return None
