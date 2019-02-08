4d8cb9d181ca12c7414d0cac7873b9b1fa9f2917
Misikir <mikireyob@gmail.com>
2019-02-08 15:28:47 -0500

    modified put_job & get_job methods in Queue class

--- server_resources.py
+++ server_resources.py
@@ -7,11 +7,14 @@ class Queue():
     def __init__(self, queue_id):
         self._queue_id = queue_id
         self._queue = deque()
-    def put_job(self, job):
-        self._queue.append(job)
+    def put_job(self, job, new_job=True):
+        if new_job:
+            self._queue.appendleft(job)
+        else:
+            self._queue.append(job)
     def get_job(self):
         try:
-            return self._queue.popleft()
+            return self._queue.pop()
         except IndexError:
             return None
 
