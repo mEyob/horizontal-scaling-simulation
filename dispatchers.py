cc7069ff69bd1d0103087e6412b11cedc1a2abb6
Misikir <mikireyob@gmail.com>
2019-02-15 20:55:56 -0500

    Minor changes in LoadBalancer class

--- dispatchers.py
+++ dispatchers.py
@@ -4,16 +4,17 @@ class LoadBalancer():
     id_seq = 1
     def __init__(self, servers):
         self.lb_id = 'lb' + str(LoadBalancer.id_seq)
-        self.servers = servers
+        self.target_servers = servers
     def jsq(self):
-        queue_len = {server.server_id: server.queue.queue_length() for server in self.servers}
-        return min(queue_len, key=queue_len.get)
+        while True:
+            queue_len = {server.server_id: server.queue.queue_length() for server in self.target_servers}
+            yield min(queue_len, key=queue_len.get)
     def roundrobin(self):
-        server_id = [server.server_id for server in self.servers]
+        server_id = [server.server_id for server in self.target_servers]
         while True:
             for i in server_id:
                 yield i
     def random(self):
-        server_id = [server.server_id for server in self.servers]
+        server_id = [server.server_id for server in self.target_servers]
         while True:
             yield random.choice(server_id)
