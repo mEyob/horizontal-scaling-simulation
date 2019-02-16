725abd72c807c137542fbd8261bdb23f418ca749
Misikir <mikireyob@gmail.com>
2019-02-16 01:05:26 -0500

    Minor rsc_id variable related changes in server_resources.py

--- server_resources.py
+++ server_resources.py
@@ -1,11 +1,12 @@
 from collections import deque, namedtuple
+from math import inf
 
 Event = namedtuple('Event', 'rsc rsc_id type ev_time')
 
 
 class Queue():
     def __init__(self, queue_id):
-        self._queue_id = queue_id
+        self.rsc_id = queue_id
         self._queue = deque()
     def put_job(self, job, new_job=True):
         if new_job:
@@ -20,38 +21,38 @@ class Queue():
     def queue_length(self):
         return len(self._queue)
     def __repr__(self):
-        return 'Queue(id={!r} , Queue length={!r})'.format(self._queue_id, self.queue_length())
+        return 'Queue(id={!r} , Queue length={!r})'.format(self.rsc_id, self.queue_length())
 
 
 class Worker():
     def __init__(self, worker_id, capacity):
-        self._worker_id = worker_id
+        self.rsc_id = worker_id
         self._capacity = capacity
         self.state = 'u' # u for unavailable
     def process_job(self, job, current_time):
         self.state = 'w' # w for working
         self.job = job
-        return Event('worker', self._worker_id, 'job_complete', current_time + (job.get_size() / self._capacity))
+        return Event('worker', self.rsc_id, 'job_complete', current_time + (job.get_size() / self._capacity))
     def __repr__(self):
-        return 'Worker(id={!r} , Capacity={!r}, State={!r})'.format(self._worker_id, self._capacity, self.state)
+        return 'Worker(id={!r} , Capacity={!r}, State={!r})'.format(self.rsc_id, self._capacity, self.state)
 
 
 class Server(): 
     id_seq = 1
     def __init__(self, launch_delay,num_of_workers, worker_capacity):
-        self.server_id = 'S' + str(Server.id_seq)
+        self.rsc_id = 'S' + str(Server.id_seq)
         Server.id_seq += 1
         self.launch_delay = launch_delay
-        self.queue = Queue(self.server_id + 'Q')
+        self.queue = Queue(self.rsc_id + 'Q')
         self.workers = {}
         self.idle_worker_reg = deque()
         for w_id in range(1, num_of_workers + 1):
-            worker_id = self.server_id + 'W' + str(w_id)
+            worker_id = self.rsc_id + 'W' + str(w_id)
             self.workers[worker_id] = Worker(worker_id, worker_capacity)
         self.state = 'stopped'
     def start(self, current_time):
         self.state = 'launching'
-        return Event('server', self.server_id, 'launch_complete', current_time + self.launch_delay)
+        return Event('server', self.rsc_id, 'launch_complete', current_time + self.launch_delay)
     def get_worker(self):
         try:
             return self.idle_worker_reg.popleft()
@@ -65,6 +66,7 @@ class Server():
                 return self.workers[worker_id].process_job(job, current_time)
             else:
                 self.queue.put_job(job, new_job=False)
+        return Event('server', self.rsc_id, 'dummy_event', inf)
     def event_handler(self, event):
         if event.type == 'launch_complete':
             self.state = 'ready'
@@ -82,5 +84,5 @@ class Server():
             next_event = self.assign_job(event.ev_time)
         return next_event
     def __repr__(self):
-        return 'Server(id={!r} , Workers={!r}, State={!r})'.format(self.server_id, len(self.workers), self.state)
+        return 'Server(id={!r} , Workers={!r}, State={!r})'.format(self.rsc_id, len(self.workers), self.state)
 
